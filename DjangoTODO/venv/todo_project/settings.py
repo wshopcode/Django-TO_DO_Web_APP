162a19bb69e3121be500e04b254db9ca41e3562d
Wakweika Felix (wshopcode) <99200275+wshopcode@users.noreply.github.com>
2023-01-07 21:03:38 +0300

    Update settings.py

--- DjangoTODO/venv/todo_project/settings.py
+++ DjangoTODO/venv/todo_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-77m+%f&a&a1#%_))cbcv*e7q90v7qg9avce$rq5aabyrj5j$#b'
+SECRET_KEY = 'INSERT SECRET KEY HERE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
